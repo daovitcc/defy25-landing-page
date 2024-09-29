01a59a886f032c5eaac0b775fcbe7be9a3807775
Akkilesh-A <akkilalagar05@gmail.com>
2024-09-29 23:51:29 +0530

    feat: add custom component

---
+++ src/components/CustomComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CustomComponent = () => {
+  return (
+    <div className=''>
+        <h1 className='font-semibold text-xl '>DeFy25 Component 1</h1>
+        <ul>
+            <li><span className='font-bold'>CustomComponent.tsx</span> Follow this convention of naming Components</li>
+            <li>Import and export components through components/index.tsx</li>
+            <li>Abstract most of the sections by creating components!</li>
+        </ul> 
+    </div>
+  )
+}
+
+export default CustomComponent
---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+//Import and export components from here
+import CustomComponent from "./CustomComponent";
+
+export { CustomComponent };
