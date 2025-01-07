c4b1801f59e43e2f81354a3992275d26ad5e1f8b
Akkilesh-A <akkilalagar05@gmail.com>
2025-01-07 11:33:33 +0530

    chore: refactor timer, add bg image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,8 +39,7 @@ export default function Timer() {
       <div className='flex justify-center items-center h-[90vh]'>
         {Object.keys(timeLeft).length > 0 ? (
           <div className='text-8xl space-x-8'>
-            <span className='bg-white p-4 rounded-xl'>{timeLeft.days<10 && "0"}{timeLeft.days}d</span>
-            <span className='bg-white p-4 rounded-xl'>{timeLeft.hours<10 && "0"}{timeLeft.hours}h</span>
+            <span className='bg-white p-4 rounded-xl'>{timeLeft.hours + timeLeft.days*24<10 && "0"}{timeLeft.hours + timeLeft.days*24}h</span>
             <span className='bg-white p-4 rounded-xl'>{timeLeft.minutes<10 && "0"}{timeLeft.minutes}m</span>
             <span className='bg-white p-4 rounded-xl'>{timeLeft.seconds<10 && "0"}{timeLeft.seconds}s</span>
           </div>
