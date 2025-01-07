f28b09e49cf1db286922fd3de828fac5fd8ca0bf
Akkilesh-A <akkilalagar05@gmail.com>
2025-01-07 09:26:17 +0530

    feat: add timer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,13 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const yapariBold = localFont({
+  src:[{
+    path: './fonts/Yapari-Bold.ttf',
+    style: 'bold',
+  }],
+  variable: "--font-yapari-bold",
+  
 });
 
 export const metadata: Metadata = {
@@ -26,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${yapariBold.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,48 @@
-import { CustomComponent } from "@/components";
+'use client'
 
-export default function Home() {
+import { useEffect, useState } from 'react';
+
+const calculateTimeLeft = () => {
+  const difference = +new Date('2025-01-11T11:00:00') - +new Date();
+  let timeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+
+  return timeLeft;
+};
+
+export default function Timer() {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
   return (
-    <div>
-      <h1>Home</h1>
-      <CustomComponent />
+    <div className='font-yapariBold'>
+      <div className='flex justify-center items-center h-[90vh]'>
+        {Object.keys(timeLeft).length > 0 ? (
+          <div className='text-8xl space-x-8'>
+            <span className='bg-white p-4 rounded-xl'>{timeLeft.days}d </span>
+            <span className='bg-white p-4 rounded-xl'>{timeLeft.hours}h </span>
+            <span className='bg-white p-4 rounded-xl'>{timeLeft.minutes}m </span>
+            <span className='bg-white p-4 rounded-xl'>{timeLeft.seconds}s</span>
+          </div>
+        ) : (
+          <span>Event has started!</span>
+        )}
+      </div>
     </div>
   );
 }
